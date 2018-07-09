259365c2a3bf157a05eb27369710fc320a362a50
Zi-Wen Lin <zw.lin1998@hotmail.com>
2018-07-09 17:23:52 +0200

    Update 1.0.4
    Pagina's hebben titels.
    Progresbalk aan de bovenkant van de pagina.
    Knoppen zijn nu compacter, gebruiken minder ruimte.

--- script.js
+++ script.js
@@ -8,6 +8,26 @@ function accordion() {
   }
 }
 
+function scrollbar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  if(document.getElementById("progress-bar")) {
+    if (window.pageYOffset > sticky) {
+      scrollbar.classList.add("sticky");
+    } else {
+      scrollbar.classList.remove("sticky");
+    }
+    document.getElementById("progress-bar").style.width = scrolled + "%";
+  }
+}
+
+if (document.getElementById("progress-container")) {
+  window.onscroll = scrollbar;
+  var scrollbar = document.getElementById("progress-container");
+  var sticky = scrollbar.offsetTop;
+}
+
 var acc = document.getElementsByClassName("accordion");
 for (var i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", accordion);
