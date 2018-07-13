7620fcc25e642e776e2954897fd7c893bed7b318
Zi-Wen Lin <zw.lin1998@hotmail.com>
2018-07-13 16:20:17 +0200

    Update 1.0.6
    Optimalisatie JavaScript code.
    Verbetering telefoon support.
    Class accordion heet nu collapse.
    Versnelling van animaties. Het voelt nu nog sneller aan.
    Oplossing panelhoogte bij een venstergrootte verandering.
    Klein beetje meer ruimte bij de randen van het scherm

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-function accordion() {
+/* Begin Initial */
+
+/* Check if there is an scroll-progress-bar */
+var progressContainer = document.getElementById("progress-container");
+if (progressContainer) {
+  addEvent(window, "scroll", onScroll);
+  var sticky = progressContainer.offsetTop;
+}
+
+addEvent(window, "resize", onResize);
+
+var collapses = document.getElementsByClassName("collapse");
+for (var i = 0; i < collapses.length; i++) {
+  addEvent(collapses[i], "click", togglecollapse);
+}
+
+/* End initial ******* Begin functions */
+
+function addEvent(object, type, callback) {
+  if (object == null || typeof(object) == 'undefined') return;
+  if (object.addEventListener) {
+    object.addEventListener(type, callback, false);
+  } else if (object.attachEvent) {
+    object.attachEvent("on" + type, callback);
+  } else {
+    object["on"+type] = callback;
+  }
+}
+
+function onResize() {
+  var panels = document.getElementsByClassName("panel");
+  if (panels) {
+    for (var i = 0; i < panels.length; i++) {
+      var panel = panels[i];
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    }
+  }
+}
+
+function togglecollapse() {
   this.classList.toggle("active");
   var panel = this.nextElementSibling;
   if (panel.style.maxHeight) {
@@ -6,30 +47,19 @@ function accordion() {
   } else {
     // panel.style.transitionDuration = Math.sqrt(panel.scrollHeight/4)/100 + "s";
     panel.style.maxHeight = panel.scrollHeight + "px";
+    // panel.style.maxHeight = "none";
   }
 }
 
-function scrollbar() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  if(document.getElementById("progress-bar")) {
-    if (window.pageYOffset > sticky) {
-      scrollbar.classList.add("sticky");
-    } else {
-      scrollbar.classList.remove("sticky");
-    }
-    document.getElementById("progress-bar").style.width = scrolled + "%";
+function onScroll() {
+  var docElm = document.documentElement;
+  var winScroll = document.body.scrollTop || docElm.scrollTop;
+  var height = docElm.scrollHeight - docElm.clientHeight;
+  var progress = (winScroll / height) * 100;
+  document.getElementById("progress-bar").style.width = progress + "%";
+  if (winScroll > sticky) {
+    progressContainer.classList.add("sticky");
+  } else {
+    progressContainer.classList.remove("sticky");
   }
-}
-
-if (document.getElementById("progress-container")) {
-  window.onscroll = scrollbar;
-  var scrollbar = document.getElementById("progress-container");
-  var sticky = scrollbar.offsetTop;
-}
-
-var acc = document.getElementsByClassName("accordion");
-for (var i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", accordion);
 }
